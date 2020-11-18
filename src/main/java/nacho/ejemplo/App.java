3f367bd96018356242a4c88bd5f588f7197217c5
Nacho <7736020@ufv.es>
2020-11-18 19:30:20 +0100

    Initial commit

---
+++ src/main/java/nacho/ejemplo/App.java
@@ -0,0 +1,13 @@
+package nacho.ejemplo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
